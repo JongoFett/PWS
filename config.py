cd79eaa8e95a76fea443b01265b89f6094736fba
JongoFett <karl.mark.c@gmail.com>
2017-09-20 01:03:29 -0700

    Added a process to check whether python 2.7 or 3 is running the show.

--- config.py
+++ config.py
@@ -1,13 +1,41 @@
 #!/usr/bin/python3
+#!/usr/bin/python
 
 #This creates the config file used to hold & read the MAC address
-import configparser
+#import configparser
+import os
+import sys
+import subprocess
+
+#This is the subprocess which gets and returns the systems Py version
+def run_command(command):
+    p = subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    return iter(p.stdout.readline, b'')
+
+if __name__ == '__main__':
+    command = 'python -V'.split()
+for line in run_command(command):
+    line = line [0:10]
+
+#If statement to check the version against a set parameter
+py_v = 'Python 2.7'
+if line == py_v:
+    import ConfigParser
+else:
+    import configparser
+#This is what allows the wol.py file to run with Py v2 or v3
+if line == py_v:
+    config = ConfigParser.ConfigParser()
+else:
+    config = configparser.ConfigParser()
+
 
 #ask user the broadcast address
 broad_cast = input("What is your broadcast address? ")
 
 #.ini settings
-config = configparser.ConfigParser()
 config['DEFAULT'] = {'ServerAliveInterval': '45',
                       'Compression': 'yes',
                       'CompressionLevel': '9'}
