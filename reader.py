988e04c19b585497ec1545b21327a562389fb781
JongoFett <karl.mark.c@gmail.com>
2017-10-04 00:13:14 -0700

    Taking the macs and making them a list.

--- reader.py
+++ reader.py
@@ -6,4 +6,6 @@ import re
 c_reader = csv.reader(open('./output/macs.csv', 'rb'), delimiter=',')
 
 col_2 = list(zip(*c_reader))[1]
-print(col_2)
+mac_list = col_2[1::]
+for macs in mac_list:
+    print(macs)
